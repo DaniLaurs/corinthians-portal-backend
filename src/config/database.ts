a48a572b296a788e926255161cead533778331f7
DaniLaurs <sousal22@outlook.com>
2026-03-10 16:36:24 -0300

    config banco supabase

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,16 +1,10 @@
-import pkg from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const { Pool } = pkg;
-
 export const db = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
