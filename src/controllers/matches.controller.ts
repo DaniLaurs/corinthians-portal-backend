dd51a8dc5525a091a681cac5fca299f3f79310a3
DaniLaurs <sousal22@outlook.com>
2026-04-17 19:51:01 -0300

    fix time

--- src/controllers/matches.controller.ts
+++ src/controllers/matches.controller.ts
@@ -13,19 +13,21 @@ export const getMatches = async (req: Request, res: Response) => {
   }
 };
 
-// CRIAR
 export const createMatch = async (req: Request, res: Response) => {
-  const { home_team, away_team, match_date, competition } = req.body;
+  const { home_team, away_team, date, time, competition } = req.body;
 
   try {
     const result = await db.query(
-      `INSERT INTO matches (home_team, away_team, match_date, competition)
-       VALUES ($1, $2, $3, $4) RETURNING *`,
-      [home_team, away_team, match_date, competition]
+      `INSERT INTO matches (home_team, away_team, date, time, competition)
+       VALUES ($1, $2, $3, $4, $5)
+       RETURNING *,
+       CONCAT(date, 'T', time) AS match_date`,
+      [home_team, away_team, date, time, competition]
     );
 
     res.status(201).json(result.rows[0]);
   } catch (err) {
+    console.log("ERRO CREATE MATCH:", err);
     res.status(500).json({ error: "Erro ao criar jogo" });
   }
 };
@@ -33,18 +35,26 @@ export const createMatch = async (req: Request, res: Response) => {
 // EDITAR
 export const updateMatch = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { home_team, away_team, match_date, competition } = req.body;
+  const { home_team, away_team, date, time, competition } = req.body;
 
   try {
     const result = await db.query(
       `UPDATE matches
-       SET home_team=$1, away_team=$2, match_date=$3, competition=$4
-       WHERE id=$5 RETURNING *`,
-      [home_team, away_team, match_date, competition, id]
+       SET 
+         home_team = $1,
+         away_team = $2,
+         date = $3,
+         time = $4,
+         competition = $5
+       WHERE id = $6
+       RETURNING *,
+       CONCAT(date, 'T', time) AS match_date`,
+      [home_team, away_team, date, time, competition, id]
     );
 
     res.json(result.rows[0]);
   } catch (err) {
+    console.log("ERRO UPDATE MATCH:", err);
     res.status(500).json({ error: "Erro ao atualizar jogo" });
   }
 };
